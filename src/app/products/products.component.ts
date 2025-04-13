0c2cc94f6752c5b7db6d2576bf1a8d70c2184a94
Aminuv <mohamed.elmarbouh07@gmail.com>
2025-04-13 21:54:31 +0100

    Add Bootstrap integration and implement Products and Customers components
    - Updated angular.json to include Bootstrap CSS and JS files.
    - Defined routes for Products and Customers components in app-routing.module.ts.
    - Enhanced app.component.html with a responsive Bootstrap navbar.
    - Improved products.component.html to display a list of products with delete functionality.
    - Implemented products.component.ts to manage product data and deletion logic.
    - Created customers.component.html and customers.component.ts for customer management.
    - Added unit tests for CustomersComponent.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -6,6 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
-
+export class ProductsComponent implements OnInit {
+handlDeleteProduct(_t18: any) {
+throw new Error('Method not implemented.');
+}
+  products! : Array<any>;
+  constructor() { }
+  ngOnInit(): void {
+    this.products = [
+      { id: 1, name: 'Product 1', price: 100 },
+      { id: 2, name: 'Product 2', price: 200 },
+      { id: 3, name: 'Product 3', price: 300 },
+      { id: 4, name: 'Product 4', price: 400 },
+      { id: 5, name: 'Product 5', price: 500 }
+    ];
+  }
+  handleDeleteProduct(product: any) {
+   let index = this.products.indexOf(product);
+   this.products.splice(index, 1);
+  }
 }
