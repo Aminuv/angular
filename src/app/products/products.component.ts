b17aefb51f9beafa8a4fefec0b667d69366f69d4
Aminuv <mohamed.elmarbouh07@gmail.com>
2025-04-14 02:47:32 +0100

    Implement ProductService and update ProductsComponent to fetch products from the service

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,3 +1,4 @@
+import { ProductService } from './../services/product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,16 +12,18 @@ handlDeleteProduct(_t18: any) {
 throw new Error('Method not implemented.');
 }
   products! : Array<any>;
-  constructor() { }
+
+  constructor( private ProductService : ProductService) { }
+
   ngOnInit(): void {
-    this.products = [
-      { id: 1, name: 'Product 1', price: 100 },
-      { id: 2, name: 'Product 2', price: 200 },
-      { id: 3, name: 'Product 3', price: 300 },
-      { id: 4, name: 'Product 4', price: 400 },
-      { id: 5, name: 'Product 5', price: 500 }
-    ];
+    this.ProductService.getAllProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+      }
+    }); 
   }
+
+
   handleDeleteProduct(product: any) {
    let index = this.products.indexOf(product);
    this.products.splice(index, 1);
