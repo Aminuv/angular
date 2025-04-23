7b489d2fdf3620d0153bced0c13f08023073d90f
Aminuv <mohamed.elmarbouh07@gmail.com>
2025-04-23 12:10:55 +0100

    Add admin template component with navigation bar and authentication integration

--- src/app/admin-template/admin-template.component.ts
+++ src/app/admin-template/admin-template.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 @Component({
   selector: 'app-admin-template',
   standalone: false,
   templateUrl: './admin-template.component.html',
   styleUrl: './admin-template.component.scss'
 })
-export class AdminTemplateComponent {
+export class AdminTemplateComponent implements OnInit {
+
+  constructor(public authService: AuthenticationService) { }
+
+  ngOnInit(): void {
+  }
 
 }
